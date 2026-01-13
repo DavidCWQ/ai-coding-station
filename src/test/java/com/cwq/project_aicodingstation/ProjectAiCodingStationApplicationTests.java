8d1f73d460f22d8f638bade409676eba2c9ac604
DavidCWQ <caowenqi4812@163.com>
2026-01-13 20:08:53 +0800

    test: update initial tests

---
+++ src/test/java/com/cwq/project_aicodingstation/ProjectAiCodingStationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cwq.project_aicodingstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAiCodingStationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
