e095f278d3cc05e78eb1d12c77f5b040149b912a
DavidCWQ <caowenqi4812@163.com>
2026-03-27 21:23:12 +0800

    fix: update test case

--- src/test/java/com/cwq/project_aicodingstation/AICodeGeneratorFacadeTest.java
+++ src/test/java/com/cwq/project_aicodingstation/AICodeGeneratorFacadeTest.java
@@ -20,7 +20,7 @@ class AICodeGeneratorFacadeTest {
     @Test
     void generateAndSaveCode() {
         File file = aiCodeGeneratorFacade.generateAndSaveCode(
-                "任务记录网站", CodeGenTypeEnum.HTML, 1L
+                "任务记录网站", CodeGenTypeEnum.HTML, 1L, 1L
         );
         Assertions.assertNotNull(file);
     }
@@ -28,7 +28,7 @@ class AICodeGeneratorFacadeTest {
     @Test
     void generateAndSaveCodeStream() {
         Flux<String> codeStream = aiCodeGeneratorFacade.generateAndSaveCodeStream(
-                "任务记录网站", CodeGenTypeEnum.MULTI_FILE, 1L
+                "任务记录网站", CodeGenTypeEnum.MULTI_FILE, 1L, 1L
         );
         // 阻塞等待所有数据收集完成
         List<String> result = codeStream.collectList().block();
