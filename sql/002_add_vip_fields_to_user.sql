b57ac7eb08c87d719ad9e64ed77b39020fb06d74
DavidCWQ <caowenqi4812@163.com>
2026-04-15 17:07:39 +0800

    fix: agent chat system error (mysql)

--- sql/002_add_vip_fields_to_user.sql
+++ sql/002_add_vip_fields_to_user.sql
@@ -6,8 +6,7 @@
 -- =====================================================
 USE `ai_coding_station_memo`;
 
--- 修改表
-ALTER TABLE `sys_user`
-    ADD COLUMN IF NOT EXISTS `vip_code`        VARCHAR(128) NULL COMMENT '会员兑换码',
-    ADD COLUMN IF NOT EXISTS `vip_number`      BIGINT       NULL COMMENT '会员编号',
-    ADD COLUMN IF NOT EXISTS `vip_expire_time` DATETIME     NULL COMMENT '会员过期时间';
+-- 分开执行，每个 ADD COLUMN 单独一个 ALTER TABLE
+ALTER TABLE `sys_user` ADD COLUMN IF NOT EXISTS `vip_code` VARCHAR(128) NULL COMMENT '会员兑换码';
+ALTER TABLE `sys_user` ADD COLUMN IF NOT EXISTS `vip_number` BIGINT NULL COMMENT '会员编号';
+ALTER TABLE `sys_user` ADD COLUMN IF NOT EXISTS `vip_expire_time` DATETIME NULL COMMENT '会员过期时间';
